85b553308b660799f62057551a7ace2b476bcfa9
JasonFung6 <jason.f@live.co.uk>
2019-08-17 21:22:09 +0100

    Add better quality image + update readme

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section('content')
-    <h1 class="headerPadding">Age Calculator</h1>
+    <h1 class="headerPadding">Base64 Image</h1>
     @if ($imgsrc)
-    <img src={{$imgsrc}} alt="Programming meme">
+    <img src={{$imgsrc}} alt="Programming Meme">
     @else
     <p>Nothing to see here!</p>
     @endif
